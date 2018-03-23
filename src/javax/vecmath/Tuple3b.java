0a2812a2a2b148494d0c429b813c3f1072f74349
victorhantelmann <victorhantelmann@gmail.com>
2018-03-23 19:56:59 +0200

    Update Tuple3b.java

--- src/javax/vecmath/Tuple3b.java
+++ src/javax/vecmath/Tuple3b.java
@@ -36,7 +36,7 @@ package javax.vecmath;
  * bytes were unsigned.
  * Values greater than 127 can be assigned to a byte variable using a
  * type cast.  For example:
- * <ul>byteVariable = (byte) intValue; // intValue can be > 127</ul>
+ * <p>byteVariable = (byte) intValue; // intValue can be > 127</p>
  * If intValue is greater than 127, then byteVariable will be negative.  The
  * correct value will be extracted when it is used (by masking off the upper
  * bits).
